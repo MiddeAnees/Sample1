0c97a7ee0f5dd40a79b55c928ed37ebb14e34cc4
MiddeAnees <130051750+MiddeAnees@users.noreply.github.com>
2023-04-18 12:52:27 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a Number"))
+b=int(input("Enter b Number"))
+c=int(input("Enter c Number"))
+if  a>b AND a>c :
+  print("a is big")
+elif b>a AND b>c :
+  print("b is Big")
+else:
+  print("c is big")
